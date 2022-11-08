320caa3af9e81bab00dc753436ebb46b581a3204
Alisher <alisher.anuarbekov@gmail.com>
2022-11-08 23:51:56 +0600

    Add column and row counter + String detect support

--- src/splat/lexer/Lexer.java
+++ src/splat/lexer/Lexer.java
@@ -19,7 +19,7 @@ public class Lexer {
 		try {
 
 			Scanner scanner = new Scanner(progFile);
-
+			int row = 1;
 			Stack<Character> quote = new Stack<>();
 			StringBuilder operator = new StringBuilder();
 			StringBuilder word = new StringBuilder();
@@ -47,28 +47,33 @@ public class Lexer {
 
 				String[] separated = line.split(" ");
 
+				int column = 1;
+
 				for (String e : separated) {
+					boolean space = true;
 
 					for (int i = 0; i < e.length(); i++) {
 
+						word.append(space && !quote.isEmpty() ? " " : "");
+
 						if (e.charAt(i) == ' ' || e.charAt(i) == '\t') continue;
 
-						if (specialCharacters.contains(e.charAt(i))) {
-							addWordOrOperator(operator, word, tokens);
-							tokens.add(new Token(e.charAt(i) + "", 0,0));
+						if (quote.isEmpty() && specialCharacters.contains(e.charAt(i))) {
+							addWordOrOperator(operator, word, tokens, quote, row, column);
+							tokens.add(new Token(e.charAt(i) + "", row, column));
 						} else if (Arrays.asList((char)34, (char)39).contains(e.charAt(i)) && quote.isEmpty()) {
-							tokens.add(new Token(e.charAt(i) + "", 0,0));
 							quote.push(e.charAt(i));
 						} else if (Arrays.asList((char)34, (char)39).contains(e.charAt(i))) {
 
 							if (quote.isEmpty())
-								throw new LexException("Closing column does not have opening column", 0, 0);
+								throw new LexException("Closing column does not have opening column", row, column);
 							else if (e.charAt(i) == (char)34 && quote.peek() != (char)34)
-								throw new LexException("Closing column does not match opening column", 0, 0);
+								throw new LexException("Closing column does not match opening column", row, column);
 							else if (e.charAt(i) == (char)39 && quote.peek() != (char)39)
-								throw new LexException("Closing column does not match opening column", 0, 0);
+								throw new LexException("Closing column does not match opening column", row, column);
 							else {
-								tokens.add(new Token(e.charAt(i)+"", 0,0));
+								tokens.add(new Token( quote.peek() + word.toString() + quote.peek() + "", row, column-word.length()+1));
+								word.setLength(0);
 							}
 
 							quote.pop();
@@ -76,29 +81,30 @@ public class Lexer {
 						} else if ((operators.contains(e.charAt(i)) || e.charAt(i) == '=') && quote.isEmpty()) {
 
 							if (complexOperators.contains(operator.toString())) {
-								tokens.add(new Token(operator + "", 0, 0));
+								tokens.add(new Token(operator + "", row, column-operator.length()+1));
 								operator.setLength(0);
 							}
 
 							operator.append(e.charAt(i));
-						} else if (allowedCharacters.get(e.charAt(i))) {  //Character.isLetterOrDigit(e.charAt(i)) || allowed.contains(e.charAt(i))
+						} else if (!quote.isEmpty()) {
+							word.append(e.charAt(i));
+						}else if (allowedCharacters.get(e.charAt(i))) {  //Character.isLetterOrDigit(e.charAt(i)) || allowed.contains(e.charAt(i))
 							word.append(e.charAt(i));
 						}  else if (!allowedCharacters.get(e.charAt(i)) && quote.isEmpty()){
-							throw new LexException("Not allowed character " + e.charAt(i), 0 ,0);
+							throw new LexException("Not allowed character " + e.charAt(i), row, column);
 						}
 
-
+						space = false;
+						column++;
 					}
 
-					addWordOrOperator(operator, word, tokens);
-
-
+					addWordOrOperator(operator, word, tokens, quote, row, column);
 
 				}
 
-				if (!quote.isEmpty()) throw new LexException("Quote has not been closed", 0 ,0);
+				if (!quote.isEmpty()) throw new LexException("Quote has not been closed", row, column);
 
-
+				row++;
 			}
 
 
@@ -111,23 +117,25 @@ public class Lexer {
 
 	}
 
-	private void addWordOrOperator(StringBuilder operator, StringBuilder word, LinkedList<Token> tokens) throws LexException {
+	private void addWordOrOperator(StringBuilder operator, StringBuilder word,
+								   LinkedList<Token> tokens, Stack<Character> quote,
+									int row, int column) throws LexException {
 
 		if (operator.length() > 0) {
 
 			if (operator.toString().equals("=") && !tokens.getLast().getValue().equals(":")) {
-				throw new LexException("Equal operator not supported", 0, 0);
+				throw new LexException("Equal operator not supported", row, column);
 			}
 
-			tokens.add(new Token(operator.toString(), 0 ,0));
+			tokens.add(new Token(operator.toString(), row ,column-operator.length()+1));
 			operator.setLength(0);
 		}
 
-		if (word.length() > 0) {
+		if (word.length() > 0 && quote.isEmpty()) {
 			if (Character.isDigit(word.charAt(0)) && Character.isAlphabetic(word.charAt(word.length()-1))) {
-				throw new LexException("Variable starts with number", 0 ,0);
+				throw new LexException("Variable starts with number", row , column-word.length()+1);
 			}
-			tokens.add(new Token(word.toString(), 0 ,0));
+			tokens.add(new Token(word.toString(), row ,column-word.length()+1));
 			word.setLength(0);
 		}
 	}
